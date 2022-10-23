3ca9187d397ad6b75aa17a2bf307d50bea1c02ef
Alvaro Clemente <clemente.varo@gmail.com>
2022-10-24 00:18:45 +0200

    custom models

---
+++ models/staging/stg_customers.sql
@@ -0,0 +1,5 @@
+SELECT
+    id AS customer_id,
+    first_name,
+    last_name
+FROM jaffle_shop_customers
---
+++ models/staging/stg_orders.sql
@@ -0,0 +1,6 @@
+SELECT
+    id AS order_id,
+    user_id as customer_id,
+    order_date,
+    status
+FROM jaffle_shop_orders
